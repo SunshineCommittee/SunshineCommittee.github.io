987b11d14d4f3c1cc993eefa71d35fc34e71ac42
FomTarro <twfarro@wpi.edu>
2018-02-14 00:03:12 -0500

    line decorations

---
+++ _posts/2018-01-11-an-older-post.markdown
@@ -0,0 +1,6 @@
+---
+title:  "An Old Post"
+---
+Here's an example of an older post, for testing with more than twp posts. It's not a real post, but that's okay.
+
+<!--more-->
--- _site/javascripts/background.js
+++ _site/javascripts/background.js
@@ -1,10 +1,24 @@
+var sidebar = $("#sidebar");
+var content = $("#content-body")
+
+function resizeCols(){
+    if(sidebar.height() >= content.height()){
+        content.height(sidebar.height());
+    }
+    else{
+        sidebar.height(content.height());
+    }
+}
+
 var bg = $("#bg");
 
 function resizeBackground() {
     bg.height($(document).height());
-    console.log("resizing");
+    //console.log("resizing");
 }
 
+$(window).scroll(resizeBackground);
 $(window).resize(resizeBackground);
 $(document).resize(resizeBackground);
 resizeBackground();
+
